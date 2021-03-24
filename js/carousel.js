f228f1b7aa51e4d31a3cbd96aa36e09a0a693684
XanderReynolds <xane514@gmail.com>
2021-03-24 07:56:12 -0400

    Added Javascript to do the carousel

---
+++ js/carousel.js
@@ -0,0 +1,44 @@
+const slides = document.querySelectorAll('.review-item');
+
+const buttons = document.querySelectorAll('.slide-ctrl-container button');
+console.log(buttons);
+
+let current = Math.floor(Math.random() * slides.length);
+let next = current < slides.length - 1 ? current + 1 : 0;
+let prev = current > 0 ? current - 1 : slides.length -1;
+
+const dummySlides = [
+  // slide 0 
+  // slide 1 
+  // slide 2 
+  // slide 3 
+];
+
+const goToNext = () => current < slides.length - 1 ? current + 1 : 0;
+const goToPrev = () => current > 0 ? current - 1 : slides.length - 1;
+
+const goToNum = (number) => {
+  current = number;
+  next = current < slides.length - 1 ? current + 1 : 0;
+  prev = current > 0 ? current - 1 : slides.length - 1; 
+  console.log("current " + current);
+  console.log("previous " + prev);
+  console.log("next " + next);
+}
+
+/* create event listeners */
+for(let i = 0; i < buttons.length; i++) {
+   buttons[i].addEventListener('click', ()=> {
+     i == 0 ? goToNum(goToPrev()) : goToNum(goToNext());
+   })
+}
+
+// goToNext()
+// goToPrev()
+// updateIndexes(param)
+// update Css
+
+// Decide how to call prev and next
+// update variables
+// current index = new index
+// update css classes
